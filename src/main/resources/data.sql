6a489bd5394c73ccfae17a9ea0fb2116cddc9bc5
Chernosmaga <sergeychernosmaga1997@yandex.ru>
2023-08-23 21:01:15 +0300

    feat: добавлены файлы для БД

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO status (status_id, status_name)
+VALUES (1, 'СОЗДАН');
+INSERT INTO status (status_id, status_name)
+VALUES (2, 'ГОТОВИТСЯ');
+INSERT INTO status (status_id, status_name)
+VALUES (3, 'ГОТОВ');
+INSERT INTO status (status_id, status_name)
+VALUES (4, 'ПОДАН');
